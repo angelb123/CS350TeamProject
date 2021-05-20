541a1f6677f1f165f36fa063f014f83fe66f5578
angelb123 <61754410+angelb123@users.noreply.github.com>
2021-05-20 10:59:43 -0700

    Add files via upload
    parser WIP

--- CommandInterpreter.java
+++ CommandInterpreter.java
@@ -1,7 +1,68 @@
 package cs350s21project.cli;
+import java.util.*;
 
-public class CommandInterpreter {
-	public void evaluate(String command) throws RuntimeException{
-		System.out.println("hello world");
+import cs350s21project.controller.CommandManagers;
+import cs350s21project.controller.command.misc.CommandMiscExit;
+
+public class CommandInterpreter  {
+	
+	public void evaluate(String command) throws RuntimeException {
+		String str = "create window wTop top view with 200 (49*39'32# 0*10'0# 0*0'30#) (117*25'30# 0*10'0# 0*0'30#)";
+		
+		String[] parsed = parse (str);
+		
+		//String test = "hello";
+		//TO-DO
+		//System.out.println(test.charAt(0));
+		
+		
+		for(int i = 0; i < parsed.length; i ++) {
+			System.out.println(parsed[i]);
+			
+			
+		}
+		
+		String temp[] =  parseLatLong(parsed[8]);
+		for(int i = 0; i < temp.length; i ++) {
+			System.out.println(temp[i]);
+			
+			
+		}
+		
+		//CommandManagers m = new CommandManagers();
+		//m.getInstance().schedule(new CommandMiscExit(m,"test"));
 	}
+	
+	
+	private String[] parse(String command) {
+		String s[] = command.split(" ");
+		return s;
+	}
+	
+	public String[] parseLatLong(String str) {
+		
+		String s[] = new String[3];
+		
+		String temp[] = str.split(new String("\\*"));
+		s[0] = temp[0];
+		
+		temp = temp[1].split("\'");
+		
+		s[1] = temp[0];
+		
+		temp = temp[1].split("#");
+		
+		s[2] = temp[0];
+
+		return s;
+		
+		
+	} 
+	
+	
+	public String[] pCoordinates(String coords) {
+		return null ;
+	}
+	
+	
 }
