8e12dd36f2b65644af771c5243412ceacbee1571
angelbermudez <abermudez@eagles.ewu.edu>
2021-05-30 17:38:34 -0700

    added code for set command

--- cs350s21project/cli/SetInterpreter.java
+++ cs350s21project/cli/SetInterpreter.java
@@ -1,15 +1,18 @@
 package cs350s21project.cli;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import cs350s21project.controller.CommandManagers;
 import cs350s21project.controller.command.*;
+import cs350s21project.controller.command.actor.*;
 import cs350s21project.controller.command.munition.*;
 import cs350s21project.datatype.*;
+import cs350s21project.model.agents.ScoreboardEntry;
 
 public class SetInterpreter {
 	
 	private AgentID _id; 
-	private A_Command defCommand;
+	private A_Command setCommand;
 	private CommandManagers _managers;
 	
 	static public void setEvaluate(String command, String[] parsed) {
@@ -26,20 +29,79 @@ public class SetInterpreter {
 		 //todo	
 		}
 		
+		//ii.7
 		if(parsed[2].equals("speed")) {//angel
-			 //todo	
+			_id = new AgentID(parsed[1]);
+			Groundspeed speed = new Groundspeed(Double.valueOf(parsed[3]));
+			setCommand = new CommandActorSetSpeed(_managers, command, _id, speed);
 		}
-		
+		//ii.8
 		if(parsed[2].equals("altitude")||parsed[2].equals("depth")) {//angel
-			 //todo	
+			_id = new AgentID(parsed[1]);
+			Altitude altitude = new Altitude(Double.valueOf(parsed[3]));
+			setCommand = new CommandActorSetAltitudeDepth(_managers, command, _id, altitude);
 		}
 		
+		//iii.7
 		if(parsed[2].equals("load")) {//angel
-			 //todo	
+			String s = _managers.getMunitionManager().getMunition(new AgentID(parsed[4])).getCommandText();
+			AgentID theActor = new AgentID(parsed[1]);
+			AgentID theMunition = new AgentID(parsed[4]);
+			
+			String munitionCommand[] = CommandInterpreter.stringArrayToArray(s);
+			
+			ScoreboardEntry entry; //Scoreboard entry, not sure what it is and does
+			
+			
+			
+			if(munitionCommand[2].equals("shell") || munitionCommand[2].equals("bomb") ) { //its either a shell or a bomb with no sensors or fuzes
+				entry = new ScoreboardEntry(theMunition);
+			}
+			
+			if(munitionCommand[2].equals("depth_charge")) { // its a depth charge with a depth sensor fuze
+				AgentID theFuze = new AgentID(munitionCommand[6]);
+				entry = new ScoreboardEntry(theMunition, theFuze, false);
+				
+			}
+			if(munitionCommand[2].equals("torpedo")) {
+				
+				AgentID theSensor = new AgentID(munitionCommand[6]);
+				AgentID theFuze = new AgentID(munitionCommand[8]);
+				
+				entry = new ScoreboardEntry(theMunition, theSensor, theFuze);
+			}
+			
+			if(munitionCommand[2].equals("missle")) {
+				AgentID theSensor = new AgentID(munitionCommand[6]);
+				AgentID theFuze = new AgentID(munitionCommand[8]);
+				
+				entry = new ScoreboardEntry(theMunition, theSensor, theFuze);
+			}
+			
+			
+			
+			
+			setCommand = new CommandActorLoadMunition(_managers, command, theActor, theMunition);
+			 
 		}
 		
-		if(parsed[2].equals("deploy")) {//angel
-			 //todo	
+		//iii.8
+		if(parsed[2].equals("deploy")) {//angel - deploy a munition
+			AgentID theActor = new AgentID(parsed[1]);
+			AgentID theMunition = new AgentID(parsed[4]);
+			
+			setCommand = new CommandActorDeployMunition(_managers, command, theActor, theMunition);
+		}
+		
+		//iii.9
+		if(parsed[2].equals("deploy") && parsed.length > 4) { // deploying a shell 
+			AgentID theActor = new AgentID(parsed[1]);
+			AgentID theMunition = new AgentID(parsed[4]);
+			
+			AttitudeYaw azimuth = new AttitudeYaw(Double.valueOf(parsed[7]));
+			AttitudePitch elevation = new AttitudePitch(Double.valueOf(parsed[9]));
+			
+			setCommand = new CommandActorDeployMunitionShell(_managers, command, theActor, theMunition, azimuth, null);
 		}
 		
 		
@@ -47,10 +109,10 @@ public class SetInterpreter {
 	}	
 	
 	public void execute() throws RuntimeException { // - Angel
-		if(defCommand == null) {
+		if(setCommand == null) {
 			throw new RuntimeException("No Command Object was created in SetCommand Class constructor ");
 		}
-		_managers.getInstance().schedule(defCommand);
+		_managers.getInstance().schedule(setCommand);
 	}//end of execute
 	
 }
