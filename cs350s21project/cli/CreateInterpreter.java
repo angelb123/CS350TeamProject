bcac53cc04d9f0e2abe822f945bcde777a7987fd
zzzime <68975598+zzzime@users.noreply.github.com>
2021-06-04 15:55:05 -0700

    Update CreateInterpreter.java

--- cs350s21project/cli/CreateInterpreter.java
+++ cs350s21project/cli/CreateInterpreter.java
@@ -24,11 +24,187 @@ public class CreateInterpreter {
 		_managers = CommandManagers.getInstance();
 		
 		if(parsed[1].equals("window")) {//han
-		//create window id top view with size (latitude1 latitude2 latitude3) (longitude1 longitude2 longitude3)
+			//create window id top view with size (latitude1 latitude2 latitude3) (longitude1 longitude2 longitude3)
+			
+			//create window wTop top view with 200 (49*39'32# 0*10'0# 0*0'30#) (117*25'30# 0*10'0# 0*0'30#)
+			if(parsed[3].equals("top")) {
+				// create window  id top view with  size 
+				//( latitude1  latitude2  latitude3)
+				//( longitude1  longitude2  longitude3)
+				_id=new AgentID(parsed[2]);
+				int size=Integer.valueOf(parsed[6]);
+				
+				String  latitude1=parsed[7];
+				String[] lati1 = CommandInterpreter.parseLatLong(latitude1);
+				//String lati11=CommandInterpreter.pParenthesis(lati1[0]);
+				String []temp1=new String[lati1.length];
+				for(int i = 0; i < lati1.length; i ++) {
+					//System.out.println(temp[i]);
+					temp1[i]=CommandInterpreter.pParenthesis(lati1[i]);
+					
+					//System.out.println(temp1[i]);
+					
+				}
+				Latitude latitude11=new Latitude(Integer.valueOf(temp1[0]), Integer.valueOf(temp1[1]), Integer.valueOf(temp1[2]));
+				
+				
+				String  latitude2=parsed[8];
+				String[] lati2 = CommandInterpreter.parseLatLong(latitude2);
+				Latitude latitude22=new Latitude(Integer.valueOf(lati2[0]), Integer.valueOf(lati2[1]), Integer.valueOf(lati2[2]));
+				
+				
+				String  latitude3=parsed[9];
+				String[] lati3 = CommandInterpreter.parseLatLong(latitude3);
+				//String lati11=CommandInterpreter.pParenthesis(lati1[0]);
+				String []temp3=new String[lati3.length];
+				for(int i = 0; i < lati3.length; i ++) {
+					//System.out.println(temp[i]);
+					temp3[i]=CommandInterpreter.pParenthesis(lati3[i]);
+					
+					//System.out.println(temp3[i]);
+					
+				}
+				Latitude latitude33=new Latitude(Integer.valueOf(temp3[0]), Integer.valueOf(temp3[1]), Integer.valueOf(temp3[2]));
+				
+				
+				String  longitude1=parsed[10];
+				String[] long1 = CommandInterpreter.parseLatLong(longitude1);
+				//String lati11=CommandInterpreter.pParenthesis(lati1[0]);
+				String []temp4=new String[long1.length];
+				for(int i = 0; i < long1.length; i ++) {
+					//System.out.println(temp[i]);
+					temp4[i]=CommandInterpreter.pParenthesis(long1[i]);
+					
+					//System.out.println(temp4[i]);
+					
+				}
+				Longitude longitude11=new Longitude(Integer.valueOf(temp4[0]), Integer.valueOf(temp4[1]), Integer.valueOf(temp4[2]));
+				
+				String  longitude2=parsed[11];
+				String[] long2 = CommandInterpreter.parseLatLong(longitude2);
+				Longitude longitude22=new Longitude(Integer.valueOf(long2[0]), Integer.valueOf(long2[1]), Integer.valueOf(long2[2]));
+				
+				String  longitude3=parsed[12];
+				String[] long3 = CommandInterpreter.parseLatLong(longitude3);
+				//String lati11=CommandInterpreter.pParenthesis(lati1[0]);
+				String []temp6=new String[long3.length];
+				for(int i = 0; i < long3.length; i ++) {
+					//System.out.println(temp[i]);
+					temp6[i]=CommandInterpreter.pParenthesis(long3[i]);
+					
+					//System.out.println(temp6[i]);
+					
+				}
+				Longitude longitude33=new Longitude(Integer.valueOf(temp6[0]), Integer.valueOf(temp6[1]), Integer.valueOf(temp6[2]));
+				
+				
+				
+				defCommand=new  CommandViewCreateWindowTop(_managers, command, _id, size, latitude11, latitude22, latitude33, longitude11
+						, longitude22, longitude33);
+			}
 			
 			
+			if(parsed[3].equals("front")) {
+				/*create window  id front view with  size 
+				 * ( longitude1  longitude2  longitude3) 
+				 * ( altitude1  altitude2  altitude3  altitude4)*/
+				
+				// 
+				
+				_id=new AgentID(parsed[2]);
+				int size=Integer.valueOf(parsed[6]);
+				
+				String  longitude1=parsed[7];
+				String[] long1 = CommandInterpreter.parseLatLong(longitude1);
+				//String lati11=CommandInterpreter.pParenthesis(lati1[0]);
+				String []temp1=new String[long1.length];
+				for(int i = 0; i < long1.length; i ++) {
+					//System.out.println(temp[i]);
+					temp1[i]=CommandInterpreter.pParenthesis(long1[i]);
+					
+					//System.out.println(temp1[i]);
+					
+				}
+				Longitude longitude11=new Longitude(Integer.valueOf(temp1[0]), Integer.valueOf(temp1[1]), Integer.valueOf(temp1[2]));
+				
+				String  longitude2=parsed[8];
+				String[] long2 = CommandInterpreter.parseLatLong(longitude2);
+				Longitude longitude22=new Longitude(Integer.valueOf(long2[0]), Integer.valueOf(long2[1]), Integer.valueOf(long2[2]));
+				
+				String  longitude3=parsed[9];
+				String[] long3 = CommandInterpreter.parseLatLong(longitude3);
+				//String lati11=CommandInterpreter.pParenthesis(lati1[0]);
+				String []temp3=new String[long3.length];
+				for(int i = 0; i < long3.length; i ++) {
+					//System.out.println(temp[i]);
+					temp3[i]=CommandInterpreter.pParenthesis(long3[i]);
+					
+					//System.out.println(temp3[i]);
+					
+				}
+				Longitude longitude33=new Longitude(Integer.valueOf(temp3[0]), Integer.valueOf(temp3[1]), Integer.valueOf(temp3[2]));
+				
+				
+				
+				Altitude altitude1=new Altitude(Double.valueOf(CommandInterpreter.pParenthesis(parsed[10])));
+				Altitude altitude2=new Altitude(Double.valueOf(CommandInterpreter.pParenthesis(parsed[11])));
+				Altitude altitude3=new Altitude(Double.valueOf(CommandInterpreter.pParenthesis(parsed[12])));
+				Altitude altitude4=new Altitude(Double.valueOf(CommandInterpreter.pParenthesis(parsed[13])));
+				
+				
+				
+				defCommand=new CommandViewCreateWindowFront(_managers, command, _id, size, longitude11, longitude22, longitude33, altitude1, altitude2, altitude3, altitude4);
+			}
+			
+			
+			if(parsed[3].equals("side")) {
+				/*create window  id side view with  size 
+				 * ( latitude1  latitude2  latitude3) 
+				 * ( altitude1  altitude2 altitude3  altitude4)*/
+				_id=new AgentID(parsed[2]);
+				int size=Integer.valueOf(parsed[6]);
+				
+				String  latitude1=parsed[7];
+				String[] lati1 = CommandInterpreter.parseLatLong(latitude1);
+				//String lati11=CommandInterpreter.pParenthesis(lati1[0]);
+				String []temp1=new String[lati1.length];
+				for(int i = 0; i < lati1.length; i ++) {
+					//System.out.println(temp[i]);
+					temp1[i]=CommandInterpreter.pParenthesis(lati1[i]);
+					
+					System.out.println(temp1[i]);
+					
+				}
+				Latitude latitude11=new Latitude(Integer.valueOf(temp1[0]), Integer.valueOf(temp1[1]), Integer.valueOf(temp1[2]));
+				
+				
+				String  latitude2=parsed[8];
+				String[] lati2 = CommandInterpreter.parseLatLong(latitude2);
+				Latitude latitude22=new Latitude(Integer.valueOf(lati2[0]), Integer.valueOf(lati2[1]), Integer.valueOf(lati2[2]));
+				
+				
+				String  latitude3=parsed[9];
+				String[] lati3 = CommandInterpreter.parseLatLong(latitude3);
+				//String lati11=CommandInterpreter.pParenthesis(lati1[0]);
+				String []temp3=new String[lati3.length];
+				for(int i = 0; i < lati3.length; i ++) {
+					//System.out.println(temp[i]);
+					temp3[i]=CommandInterpreter.pParenthesis(lati3[i]);
+					
+					//System.out.println(temp3[i]);
+					
+					
+					
+				}
+				Latitude latitude33=new Latitude(Integer.valueOf(temp3[0]), Integer.valueOf(temp3[1]), Integer.valueOf(temp3[2]));
+				Altitude altitude1=new Altitude(Double.valueOf(CommandInterpreter.pParenthesis(parsed[10])));
+				Altitude altitude2=new Altitude(Double.valueOf(CommandInterpreter.pParenthesis(parsed[11])));
+				Altitude altitude3=new Altitude(Double.valueOf(CommandInterpreter.pParenthesis(parsed[12])));
+				Altitude altitude4=new Altitude(Double.valueOf(CommandInterpreter.pParenthesis(parsed[13])));
+				
+				defCommand= new CommandViewCreateWindowSide(_managers, command, _id, size, latitude11, latitude22, latitude33, altitude1,altitude2, altitude3, altitude4);
+			}
 		}
-		
 		
 		
 		if(parsed[1].equals("actor")) {//Dustin
